c48eb22b3a54b362ab1593f6b08d512c239704bd
Mona Magdy <66495152+monadev98@users.noreply.github.com>
2020-06-08 18:01:28 +0200

    init commit

---
+++ js/comingsoon.js
@@ -0,0 +1,9 @@
+	/*======================================
+	// Typed JS
+
+	======================================*/ 
+    	$(".text-writing").typed({
+		strings: ["Projects","Skills","Blog Posts"],
+		typeSpeed: 100,
+		loop: true
+	});
